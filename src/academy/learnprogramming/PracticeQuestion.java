b5daf644e7dbbfbb40e124070e658308305b08fe
Preeti Rawat <preetirawat711@gmail.com>
2021-04-07 14:46:37 +0530

    PracticeQuestion

---
+++ src/academy/learnprogramming/PracticeQuestion.java
@@ -0,0 +1,14 @@
+package academy.learnprogramming;
+
+public class PracticeQuestion {
+
+    public static void checkNumber(int number){
+        if(number>0){
+            System.out.println("Positive");
+        }else if(number<0){
+            System.out.println("Negative");
+        }else if(number==0){
+            System.out.println("Zero");
+        }
+    }
+}
