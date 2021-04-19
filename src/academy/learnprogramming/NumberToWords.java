4712894e26419db77a779c9efd696b0764249321
Preeti Rawat <preetirawat711@gmail.com>
2021-04-19 17:20:51 +0530

    Question#26

---
+++ src/academy/learnprogramming/NumberToWords.java
@@ -0,0 +1,9 @@
+package academy.learnprogramming;
+
+public class NumberToWords {
+    public static void numberToWords(int number){
+        if(number<1){
+            System.out.println("Invalid Value");
+        }
+    }
+}
